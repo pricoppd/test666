c85edb3ef4d763e3c532680a1e39997a4f46b74f
pricoppd <pricoppd@gmail.com>
2016-03-14 12:53:30 +0000

    Added files via upload

---
+++ passtheproxy.ps1
@@ -0,0 +1,12 @@
+﻿$proxyString = "http://10.100.20.207:8080"
+
+$proxyUri = new-object System.Uri($proxyString)
+
+[System.Net.WebRequest]::DefaultWebProxy = 
+    new-object System.Net.WebProxy ($proxyUri, $true)
+
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = 
+    [System.Net.CredentialCache]::DefaultCredentials
+
+Add-AzurermAccount
+#Set-AzureSubscription –DefaultSubscription "PAYG-Live" 
